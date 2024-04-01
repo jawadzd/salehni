404a7a108bf2a2b8c935144babe1cdd08d4a46dc
Jawad Zeyneddin <jawad.zein.deen@hotmail.com>
2024-04-01 22:57:55 +0300

    added the clock and slpit navbar into seperate file

--- main/home.php
+++ main/home.php
@@ -1,40 +1,102 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>HOME</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <title>HOME</title>
+
+  <style>
+    body {
+      background-image: url('../src/img/body.jpg');
+      background-size: cover;
+      background-position: center;
+      background-repeat: no-repeat;
+      margin: 0;
+      padding-top: 400px;
+    }
+
+    .navbar {
+      background-color: white;
+      position: fixed;
+      top: 0;
+      left: 0;
+      right: 0;
+    }
+    .nav-item{
+      border: 1px solid black;
+      border-radius: 10px;
+      margin-left:4px;
+      margin-right:4px;
+    }
+
+    .clock-container {
+      padding-top: 100px;
+      text-align: center;
+      color: white;
+
+      font-size: 24px;
+      position: fixed;
+      top: 10px;
+      right: 10px;
+      text-align: right;
+      font-size: 24px;
+
+    }
+
+    #clock {
+      font-weight: bold;
+    }
+
+    #date {
+      margin-top: 10px;
+    }
+  </style>
+ <?php
+ include("../elements/navbar.php");
+ 
+ ?>
 </head>
-<body >
-    <div style="background-image:url(src/img/pexels-cesar-perez-733745.jpg);
-    background-position: center;
-background-repeat: no-repeat;
-paddig:1000px;">
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-    </div>
+
+<body>
+
+  <div class="clock-container">
+    <div id="clock"></div>
+    <div id="date"></div>
   </div>
-</nav>
-</div>
 </body>
+
+
+<script>
+
+
+  function updateClock() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+
+    // Add leading zeros to single digit hours, minutes, and seconds
+    hours = (hours < 10 ? "0" : "") + hours;
+    minutes = (minutes < 10 ? "0" : "") + minutes;
+    seconds = (seconds < 10 ? "0" : "") + seconds;
+
+    var timeString = hours + ":" + minutes + ":" + seconds;
+    var dateString = now.toDateString();
+
+    document.getElementById("clock").textContent = timeString;
+    document.getElementById("date").textContent = dateString;
+  }
+
+  // Update the clock every second
+  setInterval(updateClock, 1000);
+
+  // Initial call to display the clock immediately
+  updateClock();
+
+
+
+</script>
 </html>
